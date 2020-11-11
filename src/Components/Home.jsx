d5606894a02747adbf983e46397105745404527a
Riccardo Cogoni <cogonir@live.com>
2020-11-11 18:39:16 +0000

    refactor / add description

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import art from '../assets/art.png';
 import '../styles/Home.css';
-import contact from '../assets/contact.png';
+import Contact from './Contact';
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="main">
-        <div className="par">
-          <p>
-            Well done mama description, done mama description, done mama
-            description, done mama description, done mama description, done mama
-            description, done mama description,{' '}
-          </p>
-        </div>
-        <img src={art} alt="art" className="wdm-art" />
-      </div>
-      <form id="form" method="post">
-        <div className="bg">
-          <img src={contact} alt="contact" className="wdm-contact" />
-          <div class="formgroup" id="name-form">
-            <label for="name">Your name*</label>
-            <input type="text" id="name" name="name" />
-          </div>
-          <div class="formgroup" id="email-form">
-            <label for="email">Your e-mail*</label>
-            <input type="email" id="email" name="email" />
-          </div>
-          <div class="formgroup" id="message-form">
-            <label for="message">Your message</label>
-            <textarea id="message" name="message"></textarea>
-          </div>{' '}
-        </div>
-      </form>
+      <div className="main"></div>
+      <Contact />
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,57 +1,15 @@
 import '../styles/NavBar.css';
 import logo from '../assets/logo.png';
-import React, { useState } from 'react';
-import {
-  Button,
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon
-} from '@material-ui/core';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
+import React from 'react';
 
-export default function NavBar(props) {
-  const [isOpen, setOpen] = useState(false);
+import art from '../assets/art.png';
 
+export default function NavBar() {
   return (
     <div className="NavBar">
+      <img src={art} alt="art" className="wdm-art" />
+
       <img src={logo} alt="logo" className="head-logo" />
-
-      {/* <Button onClick={() => setOpen(!isOpen)}>
-        <MenuIcon style={{ fontSize: '50px' }} />
-      </Button> */}
-      <Drawer anchor="right" open={isOpen}>
-        <Button onClick={() => setOpen(!isOpen)}>
-          Close
-          {/* <CloseIcon  /> */}
-        </Button>
-        <Divider />
-
-        <List component="nav" aria-label="main mailbox folders">
-          <ListItem
-            button
-            // selected={selectedIndex === 0}
-            // onClick={(event) => handleListItemClick(event, 0)}
-          >
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <ListItem
-            button
-            // selected={selectedIndex === 1}
-            // onClick={(event) => handleListItemClick(event, 1)}
-          >
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
-          </ListItem>
-        </List>
-      </Drawer>
     </div>
   );
 }
